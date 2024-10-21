38e606a7a30cfeae4f7ca5777d8c85955e20d9f0
Harning Laptop <hejthenerd@gmail.com>
2024-10-21 15:24:23 +0200

    Commented on what AlexTest does

--- src/alextest.java
+++ src/alextest.java
@@ -1,5 +1,6 @@
 public class alextest {
     public static void main(String[] args) {
+        // prints out test then success
         System.out.println("test");
         System.out.println("Success");
     }
