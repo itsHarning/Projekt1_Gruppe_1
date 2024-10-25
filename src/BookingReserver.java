13983b2559fd722d4d0c38e368c3c8b91a8f0ebf
EmilRasmussen123 <emra0004@stud.kea.dk>
2024-10-25 11:26:19 +0200

    fjernet den Tobias skulle bruge

--- src/BookingReserver.java
+++ src/BookingReserver.java
@@ -27,34 +27,6 @@ public class BookingReserver {private static boolean[] bookings = new boolean[9]
 
             Booking booking = new Booking(name, phonenumber, time);
 
-
-
-
-
-
-
-
-
-
-
-            // TODO: GIVE TO TOBIAS
-            if (tid == 0) {  // Afslut programmet
-                return;
-            } else if (tid < 10 || tid > 17) {
-                System.out.println("tidsperioden er ugyldig, vælg venligst en ny tid.");
-            } else if (bookings[tid - 10]) {
-                System.out.println(tid + "tidsperioden er desværre allerede booket, vælg venligst en ny tid.");
-            } else {
-                bookings[tid - 10] = true;
-                System.out.println("Du har booket tidsperioden "+tid+" ");
-            }
-            String phoneNum = scanner.nextLine();
-
-            System.out.println("Indtast venligst hvilken tidsperiode mellem (10-17).");
-            String time = scanner.nextLine();
-
-            Booking booking = new Booking(name, phoneNum, date, time);
-
         } catch (Exception e) {  // Hvis der opstår en fejl (f.eks. ugyldigt input)
             System.out.println(e.getMessage());
             scanner.nextLine();  // Ryd scanner-bufferen
