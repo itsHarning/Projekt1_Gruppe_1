bc835a5a60e6a9c0e0fb3911403fc75ac648be34
EmilRasmussen123 <emra0004@stud.kea.dk>
2024-10-25 11:14:13 +0200

    Opdatering ift. rettelser og fejl

--- src/BookingReserver.java
+++ src/BookingReserver.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class BookingReserver {private static boolean[] bookings = new boolean[9];  // 8 hours from the timespan 10 -17
 
     public static void main(String[] args) {
-        arrangeBooking();
+        BookingList dummy =new BookingList();
+        arrangeBooking(dummy);
     }
 
-    public static void arrangeBooking() {
+    public static void arrangeBooking(BookingList bookinglist)
+    {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("please choose a timeperiod between 10-17);
+        System.out.println("please choose a timeperiod between 10-17");
 
         try {
             System.out.println("ask date");
@@ -18,13 +20,12 @@ public class BookingReserver {private static boolean[] bookings = new boolean[9]
             String name = scanner.nextLine();
 
             System.out.print("ask phone number.");
-<<<<<<< HEAD
-            String tlfNummer = scanner.nextLine();
+            String phonenumber = scanner.nextLine();
 
             System.out.println("Indtast venligst hvilken tidsperiode mellem (10-17).");
-            int tid = scanner.nextInt();
+            String time = scanner.nextLine();
 
-            Booking booking = new Booking(navn, tlfNummer, tid);
+            Booking booking = new Booking(name, phonenumber, time);
 
 
 
@@ -47,7 +48,6 @@ public class BookingReserver {private static boolean[] bookings = new boolean[9]
                 bookings[tid - 10] = true;
                 System.out.println("Du har booket tidsperioden "+tid+" ");
             }
-=======
             String phoneNum = scanner.nextLine();
 
             System.out.println("Indtast venligst hvilken tidsperiode mellem (10-17).");
@@ -55,7 +55,6 @@ public class BookingReserver {private static boolean[] bookings = new boolean[9]
 
             Booking booking = new Booking(name, phoneNum, date, time);
 
->>>>>>> origin/main
         } catch (Exception e) {  // Hvis der opstår en fejl (f.eks. ugyldigt input)
             System.out.println(e.getMessage());
             scanner.nextLine();  // Ryd scanner-bufferen
