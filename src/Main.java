e58dacdf72eceeffa5f9be490b98b16be856e822
Harning Laptop <hejthenerd@gmail.com>
2024-10-31 13:04:56 +0100

    Made it so you can run from Main as well as HarrySalon

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        // ints are temporarily placed here for ease of function
-        // will likely be moved to a separate class later
-        int standardCut = 30;
-        int longHair = 15;
-        int shave = 15;
-        int dye = 30;
-        // formatting of the date/time can easily be changes
-        Booking booking = new Booking("Test Booking","46460046","2024 10 21 10:00");
-
-        System.out.println(booking);
+        HarrySalon.mainMenu();
     }
 }
