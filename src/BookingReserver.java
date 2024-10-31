0bdf2ca6987d8af44b5f5216d08e0813ebd2f8cb
Simon Gotfred <simongotfred@hotmail.com>
2024-10-31 22:26:02 +0100

    Re-organized order of input in BookingReserver + minor alterations to SysOut strings

--- src/BookingReserver.java
+++ src/BookingReserver.java
@@ -1,33 +1,50 @@
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
-public class BookingReserver {private static boolean[] bookings = new boolean[9];  // 8 hours from the timespan 10 -17
+public class BookingReserver {
 
     public static void main(String[] args) {
-        BookingList dummy =new BookingList();
+        BookingList dummy = new BookingList();
     }
+
     public static void arrangeBooking()
     {
         Scanner scanner = new Scanner(System.in);
         try {
-            System.out.print("indtast navn: ");
-            String name = scanner.nextLine();
-
-            System.out.print("indtast tlf.nummer:");
-            String phoneNumber = scanner.nextLine();
-
-            System.out.println("Indtast venligst dato og tid:" + Booking.formatterString);
+            System.out.println("Indtast venligst dato og tid: " + Booking.formatterString);
             String time = scanner.nextLine();
+            LocalDateTime wantTime = LocalDateTime.parse(time);
+            LocalDateTime suggestedTime = HarrySalon.bookingList.hasTimeAt(wantTime);
 
-            Booking booking = new Booking(name, phoneNumber, time);
-            if (!HarrySalon.bookingList.add(booking))
+            if (!suggestedTime.equals(wantTime)) // TODO: testing
             {
-                System.out.print("Denne tid er desværre allerede booket:");
+                if (HarrySalon.bookingList.isShopOpen(wantTime))
+                {
+                    System.out.print("Denne tid er desværre allerede booket. ");
+                }
+                else
+                {
+                    System.out.print("Denne tid er der desværre lukket. ");
+                }
+                System.out.println("Den tidligste tid derefter vil være: " + suggestedTime.format(DateTimeFormatter.ofPattern(Booking.formatterString)));
+
                 arrangeBooking();
             }
 
+            System.out.print("Indtast kundenavn: ");
+            String name = scanner.nextLine();
+
+            System.out.print("Indtast tlf.nummer: ");
+            String phoneNumber = scanner.nextLine();
+
+            Booking booking = new Booking(name, phoneNumber, time);
+            HarrySalon.bookingList.add(booking);
+
         } catch (Exception e) {  // if there is any errors it would print the error (like non-valid input)
-            System.out.println("ugyldig dato");
-            scanner.nextLine();  // clears the Buffer-scanner
+            System.out.println("Ugyldigt formatteret dato!");
+            arrangeBooking();
         }
     }
 }
